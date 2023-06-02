9331d5dfd34cd7141ef07892a1fca4b8fcc65e15
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-06-02 09:07:44 +0200

    feat: add environment configuration and file structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ResetStyle from './components/styled/Reset';
+import GlobalStyle from './components/styled/Global';
+
+const App = () => (
+	<>
+		<ResetStyle />
+		<GlobalStyle />
+		<h1>Form</h1>
+	</>
+);
+
+export default App;
