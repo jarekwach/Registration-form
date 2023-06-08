55f179cfe5249735a7350ebdcecd0a7e0140f28c
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-06-08 23:20:33 +0200

    fix: remove unused component

--- src/components/InputRadio/InputRadio.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { StyledInputRadio } from './InputRadio.styled';
-
-const InputRadio = (props) => {
-	return <StyledInputRadio {...props}></StyledInputRadio>;
-};
-
-export default InputRadio;
--- src/components/InputRadio/InputRadio.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const StyledInputRadio = styled.div`
-
-`;
-
-export { StyledInputRadio };
